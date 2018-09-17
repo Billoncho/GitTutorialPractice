5cdaa08c7519c3357db5108a81ac9fba09c5645e
Billy Ridgeway <37519033+Billoncho@users.noreply.github.com>
2018-09-18 07:54:39 +0800

    Uploaded the original files.

---
+++ Homework 2.js
@@ -0,0 +1,36 @@
+/*
+Exercise I - Selector Quiz
+Time: 20 Minutes
+
+START HERE
+
+Answer the questions below based on the HTML / CSS to the left. Do not manipulate the HTML or CSS in any way until after you have completed the quiz (no peeking!). Afterwards you may delete line 1 from the CSS to check your work (reminder this is not graded! Try and compelte this without looking).
+
+1. Examine the h3 element containing "Step 1". What is the text color? What is the background color?
+  text color = agua, background = black
+
+2. Look at the h3 element containing "Step 3". What is the text and background color of this element?
+  text color = orange, background = black
+
+3. Look at all four paragraphs. What is the text color of each of them?
+  1st. text = lightgrey, background = darkblue / 2nd. & 3rd. text = lightgrey, background = blue / 4th. text = orange, background = blue
+
+4. What is the border color around the first div element?
+  red
+
+5. What is the background color for the second div element?
+  none
+
+6. What is the background color for each of the three p elements with the class "step-text"?
+  2nd. & 3rd paragraphs have blue backgrounds and the 4th. has a blue background
+
+
+Stretch Questions:
+
+7. What is the text and background color of the h1 element?
+  default white background and black text
+
+8. Of the p elements, which is the largest (takes up the most space on the page)?
+  The first paragraph without a class. It's font is set to 20 pixels.
+
+*/
